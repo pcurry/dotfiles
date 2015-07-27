21ad82812c85ab31587171c99c4df312accf9645
Sebastian Wiesner <swiesner@lunaryorn.com>
2015-07-27 17:12:07 +0200

    [spacemacs] Port some configurations

--- spacemacs/.emacs.d/private/lunaryorn/config.el
+++ spacemacs/.emacs.d/private/lunaryorn/config.el
@@ -26,6 +26,23 @@
 
 ;;; Code:
 
+;; Additional fonts for special characters and fallbacks
+;; Test range: 🐷 ⊄ ∫ 𝛼 α 🜚
+(when (eq system-type 'darwin)
+  ;; Colored Emoji on OS X
+  (set-fontset-font t 'symbol (font-spec :family "Apple Color Emoji")
+                    nil 'prepend)
+  (set-fontset-font t 'symbol (font-spec :family "Apple Symbols") nil 'append))
+(set-fontset-font t 'mathematical (font-spec :family "XITS Math") nil 'append)
+;; Fallback for Greek characters which Source Code Pro doesn't provide.
+(set-fontset-font t 'greek (pcase system-type
+                             (`darwin (font-spec :family "Menlo"))
+                             (_ (font-spec :family "DejaVu Sans Mono")))
+                  nil 'append)
+
+;; A general fallback for all kinds of unknown symbols
+(set-fontset-font t nil (font-spec :family "Apple Symbols") nil 'append)
+
 ;; Basic configuration
 (setq line-spacing 0.1
       ;; Keep right option modifier free for accented input
@@ -34,8 +51,14 @@
 ;; Use jk to leave insert mode
 (setq evil-escape-key-sequence "jk")
 
-;; Don't nag me magit
-(setq magit-push-always-verify nil)
+;; Version control settings
+(setq magit-push-always-verify nil      ; Don't nag me magit
+      vc-follow-symlinks t              ; Follow symlinks to VC dirs
+      )
+
+;; Shut up, Dired!
+(setq dired-omit-verbose nil
+      dired-recursive-copies 'always)
 
 (provide 'config)
 ;;; config.el ends here
