bd1d1a153737b40a0ee58ec4adfb9c340689f409
Sebastian Wiesner <swiesner@lunaryorn.com>
2015-10-09 16:45:03 +0200

    [spacemacs] Try spacemacs dark theme
    I just looks so... exotic

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -122,13 +122,13 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(solarized-light
-                         solarized-dark
+   dotspacemacs-themes '(
                          spacemacs-dark
                          spacemacs-light
-                         leuven
-                         monokai
-                         zenburn)
+                         solarized-dark
+                         solarized-light
+                         zenburn
+                         )
    ;; If non nil the cursor color matches the state color.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
