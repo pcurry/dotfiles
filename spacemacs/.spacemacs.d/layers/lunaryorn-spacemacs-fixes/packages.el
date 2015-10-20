0e5eb0f63428fe4ae29988a72fe9cad4b84273fc
Sebastian Wiesner <swiesner@lunaryorn.com>
2015-10-20 10:19:36 +0200

    [spacemacs] Add prefixes for Ensime and Ruby
    See https://github.com/syl20bnr/spacemacs/pull/3483 and
    https://github.com/syl20bnr/spacemacs/pull/3482

--- spacemacs/.spacemacs.d/layers/lunaryorn-spacemacs-fixes/packages.el
+++ spacemacs/.spacemacs.d/layers/lunaryorn-spacemacs-fixes/packages.el
@@ -13,6 +13,11 @@
 (setq lunaryorn-spacemacs-fixes-packages
       '(
         company-flx
+        ensime
+        ruby-tools
+        bundler
+        robe
+        ruby-test-mode
         ))
 
 (setq lunaryorn-spacemacs-fixes-excluded-packages '())
@@ -24,3 +29,37 @@
   (spacemacs|use-package-add-hook company
     :pre-config
     (company-flx-mode)))
+
+(defun lunaryorn-spacemacs-fixes/pre-init-ensime ()
+  ;; See https://github.com/syl20bnr/spacemacs/pull/3482
+  (dolist (prefix '(("mb" . "scala/build")
+                    ("mc" . "scala/check")
+                    ("md" . "scala/debug")
+                    ("me" . "scala/errors")
+                    ("mg" . "scala/goto")
+                    ("mh" . "scala/docs")
+                    ("mi" . "scala/inspect")
+                    ("mn" . "scala/ensime")
+                    ("mr" . "scala/refactor")
+                    ("mt" . "scala/test")
+                    ("ms" . "scala/repl")))
+    (spacemacs/declare-prefix-for-mode 'scala-mode (car prefix) (cdr prefix))))
+
+;; See https://github.com/syl20bnr/spacemacs/pull/3483
+(defun lunaryorn-spacemacs-fixes/pre-init-ruby-tools ()
+  (dolist (mode '(ruby-mode enh-ruby-mode))
+    (spacemacs/declare-prefix-for-mode mode "mx" "ruby/text")))
+
+(defun lunaryorn-spacemacs-fixes/pre-init-bundler ()
+  (dolist (mode '(ruby-mode enh-ruby-mode))
+    (spacemacs/declare-prefix-for-mode mode "mb" "ruby/bundle")))
+
+(defun lunaryorn-spacemacs-fixes/pre-init-robe ()
+  (dolist (mode '(ruby-mode enh-ruby-mode))
+    (spacemacs/declare-prefix-for-mode mode "mg" "ruby/goto")
+    (spacemacs/declare-prefix-for-mode mode "mh" "ruby/docs")
+    (spacemacs/declare-prefix-for-mode mode "ms" "ruby/repl")))
+
+(defun lunaryorn-spacemacs-fixes/pre-init-ruby-test-mode ()
+  (dolist (mode '(ruby-mode enh-ruby-mode))
+    (spacemacs/declare-prefix-for-mode mode "mt" "ruby/test")))
