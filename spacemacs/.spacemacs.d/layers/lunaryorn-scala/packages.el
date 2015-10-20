a4480591d43866a89d8671cd2949f8b0dd81963d
Sebastian Wiesner <swiesner@lunaryorn.com>
2015-10-20 11:22:08 +0200

    [spacemacs] Add expand-region for Ensime

--- spacemacs/.spacemacs.d/layers/lunaryorn-scala/packages.el
+++ spacemacs/.spacemacs.d/layers/lunaryorn-scala/packages.el
@@ -12,6 +12,7 @@
     '(
       sbt-mode
       (flycheck-auto-scalastyle :location local)
+      (ensime-mode-expansions :location local)
       flycheck
       ))
 
@@ -26,6 +27,10 @@
     :init (add-hook 'scala-mode-hook #'flycheck-auto-scalastyle-setup)
     :config (setq flycheck-auto-scalastyle-jar-dir
                   spacemacs-cache-directory)))
+
+(defun lunaryorn-scala/init-ensime-mode-expansions ()
+  (with-eval-after-load 'ensime
+    (use-package ensime-mode-expansions)))
 
 (defun lunaryorn-scala/post-init-flycheck ()
   (add-hook 'flycheck-locate-config-file-functions
