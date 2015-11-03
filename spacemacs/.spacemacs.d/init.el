cdeceab1ec49616c71fe26790d795a284bcdb940
Sebastian Wiesner <swiesner@lunaryorn.com>
2015-11-03 13:42:00 +0100

    [spacemacs] Disable yasnippet
    This thing is just awfully distracting

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -103,7 +103,10 @@ values."
    ;; configuration in `dotspacemacs/config'.
    dotspacemacs-additional-packages '()
    ;; A list of packages and/or extensions that will not be install and loaded.
-   dotspacemacs-excluded-packages '()
+   dotspacemacs-excluded-packages '(
+                                    ;; Snippets are awefully distracting
+                                    yasnippet
+                                    )
    ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
    ;; are declared in a layer which is not a member of
    ;; the list `dotspacemacs-configuration-layers'. (default t)
