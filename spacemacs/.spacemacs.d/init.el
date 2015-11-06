73f664ef00736dbf9ee4cc23b57a56dca3e7ef27
Sebastian Wiesner <swiesner@lunaryorn.com>
2015-11-06 10:13:14 +0100

    [spacemacs] Switch to light themes (solarized as default)

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -146,10 +146,10 @@ values."
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
-                         spacemacs-dark
-                         ;; spacemacs-light
-                         ;; solarized-dark
                          solarized-light
+                         spacemacs-light
+                         spacemacs-dark
+                         ;; solarized-dark
                          ;; zenburn
                          )
    ;; If non nil the cursor color matches the state color.
