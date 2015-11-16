08c791106edff4290ef867f21ff5ab4bdc6d7730
Sebastian Wiesner <swiesner@lunaryorn.com>
2015-11-16 10:45:02 +0100

    [spacemacs] Define key only after mode map is defined

--- spacemacs/.spacemacs.d/layers/lunaryorn-scala/packages.el
+++ spacemacs/.spacemacs.d/layers/lunaryorn-scala/packages.el
@@ -25,8 +25,9 @@
     :config (add-hook 'thrift-mode-hook (lambda () (run-hooks 'prog-mode-hook)))))
 
 (defun lunaryorn-scala/post-init-sbt-mode ()
-  (evil-leader/set-key-for-mode 'scala-mode
-    "moi" #'lunaryorn-scala/pop-to-sbt-frame))
+  (with-eval-after-load 'scala-mode2
+    (evil-leader/set-key-for-mode 'scala-mode
+      "moi" #'lunaryorn-scala/pop-to-sbt-frame)))
 
 (defun lunaryorn-scala/post-init-flycheck ()
   (add-hook 'flycheck-locate-config-file-functions
